5071429b9e109c4210a675255d9247623f0c13c5
xiegqooo <xiegqooo@hotmail.com>
2013-11-15 10:14:48 +0800

    form 数据加载 php 返回内容
    Signed-off-by: xiegqooo <xiegqooo@hotmail.com>

---
+++ load.php
@@ -0,0 +1,14 @@
+{
+    success: true,
+    data: {
+        clientName: "Fred. Olsen Lines",
+        portOfLoading: "FXT",
+        portOfDischarge: "OSL"
+    }
+}
+<?php
+//{
+//    success: false,
+//    errorMessage: "Consignment reference not found"
+//}
+?>
