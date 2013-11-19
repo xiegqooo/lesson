e8c712a2f2c9338e695215eaa5860adf40d5013c
xiegqooo <xiegqooo@hotmail.com>
2013-11-19 09:41:06 +0800

    form 表单提交
    Signed-off-by: xiegqooo <xiegqooo@hotmail.com>

---
+++ submit.php
@@ -0,0 +1,16 @@
+{
+    success: true
+}
+<?php
+//{
+//    success: false,
+//    errorMessage: "Consignment reference not found"
+//}
+/* {
+    success: false,
+    errors: {
+        clientCode: "Client not found",
+        portOfLoading: "This field must not be null"
+    }
+} */
+?>
