6bc7da82556c26a0acce20bf0ecd93c34611d0a8
xiegqooo <xiegqooo@hotmail.com>
2013-12-24 11:38:23 +0800

    JSONP 使用 后台数据返设置
    Signed-off-by: xiegqooo <xiegqooo@hotmail.com>

---
+++ get-images.php
@@ -0,0 +1,6 @@
+stcCallback({
+    images: [
+        {name: 'Image one', url:'/GetImage.php?id=1', size:46.5, sizemod: new Date(2007, 10, 29)},
+        {name: 'Image Two', url:'/GetImage.php?id=2', size:43.2, sizemod: new Date(2007, 10, 30)}
+    ]
+});
